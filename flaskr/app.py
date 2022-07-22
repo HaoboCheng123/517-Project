fdcb41ff502ab9ec215764dc47586c8cd20cf205
Jacky <jackycheng4932@gmail.com>
2022-07-22 02:23:02 -0400

    add 5 methods to compute centralityu

--- flaskr/app.py
+++ flaskr/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify
 import networkx as nx
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from networkx.utils import groups
 
 app = Flask(__name__)
@@ -61,39 +62,178 @@ def dashboard():
                 companies.append(com)
 
         relations_edge = set(relations)
-        G = nx.Graph()
-
-        G.add_edges_from(relations_edge)
-
-        nx.set_node_attributes(G, node_color, name="group")
 
         if form_data['centrality'] == "Follower":
-            for edges in G.edges():
-                node_value[edges[1]]+=node_value[edges[0]]
+            G = nx.Graph()
 
+            G.add_edges_from(relations_edge)
+
+            nx.set_node_attributes(G, node_color, name="group")
+
+            for edges in G.edges():
+                node_value[edges[1]] += node_value[edges[0]]
             if node_value:
                 minVal = min(node_value.values())
                 maxVal = max(node_value.values())
             for key in node_value.keys():
                 node_value[key] = (node_value[key]-minVal) / (maxVal - minVal)
 
+            nx.set_node_attributes(G, node_value, name="value")
+
+            # largest_cc = max(nx.connected_components(G), key=len)
+            #
+            # print(largest_cc)
+
+            # Convert network graph to json data
+            # d = nx.json_graph.node_link_data(G)
+
+            # for each in d['links']:
+            #     if each['source'] not in largest_cc and each['target'] not in largest_cc:
+            #         each.pop('source')
+            #         each.pop('target')
+
+        elif form_data['centrality'] == "Degree":
+            G = nx.Graph()
+
+            G.add_edges_from(relations_edge)
+
+            nx.set_node_attributes(G, node_color, name="group")
+
+            centrality = nx.degree_centrality(G)
+
+            for v, c in centrality.items():
+                node_value[v] = c
+
+            # sorted(G.degree, key=lambda x: x[1], reverse=True)
+
+            # for each in G.degree(members):
+            #     node_value[each[0]] = each[1]
+            # for each in G.degree(companies):
+            #     node_value[each[0]] = each[1]
+
+            # Data Normalization
+            # if node_value:
+            #     minVal = min(node_value.values())
+            #     maxVal = max(node_value.values())
+            # for key in node_value.keys():
+            #     node_value[key] = (node_value[key] - minVal) / (maxVal - minVal)
 
             nx.set_node_attributes(G, node_value, name="value")
 
-            d = nx.json_graph.node_link_data(G)
-        elif form_data['centrality'] == "Degree":
-            print(list(nx.degree(members)))
+            # d = nx.json_graph.node_link_data(G)
+
+        elif form_data['centrality'] == "Eigen":
+            G = nx.Graph()
+
+            G.add_edges_from(relations_edge)
+
+            nx.set_node_attributes(G, node_color, name="group")
+
+            centrality = nx.eigenvector_centrality(G)
+            for v, c in centrality.items():
+                node_value[v] = c
+
+            # Data Normalization
+            # if node_value:
+            #     minVal = min(node_value.values())
+            #     maxVal = max(node_value.values())
+            # for key in node_value.keys():
+            #     node_value[key] = (node_value[key] - minVal) / (maxVal - minVal)
+
+            nx.set_node_attributes(G, node_value, name="value")
+
+        elif form_data['centrality'] == "PageRank":
+            G = nx.DiGraph()
+
+            G.add_edges_from(relations_edge)
+
+            nx.set_node_attributes(G, node_color, name="group")
+
+            centrality = nx.pagerank(G, alpha=0.9)
+
+            for v, c in centrality.items():
+                node_value[v] = c
+
+            # if node_value:
+            #     minVal = min(node_value.values())
+            #     maxVal = max(node_value.values())
+            # for key in node_value.keys():
+            #     node_value[key] = (node_value[key] - minVal) / (maxVal - minVal)
+
+            nx.set_node_attributes(G, node_value, name="value")
+        elif form_data['centrality'] == "VoteRank":
+            G = nx.DiGraph()
+
+            G.add_edges_from(relations_edge)
+
+            nx.set_node_attributes(G, node_color, name="group")
+
+            centrality = nx.voterank(G)
+
+            # if node_value:
+            #     minVal = min(node_value.values())
+            #     maxVal = max(node_value.values())
+            # for key in node_value.keys():
+            #     node_value[key] = (node_value[key] - minVal) / (maxVal - minVal)
+
+            nx.set_node_attributes(G, node_value, name="value")
+
+            # d = nx.json_graph.node_link_data(G)
+
+        # If use d3 js, then need dump json data to a file
+        # json.dump(d, open(graph_json, "w"))
+        top_companies = {}
+        rank_num = 1
+
+        if form_data['centrality'] != "VoteRank":
+            node_rank = dict(sorted(node_value.items(), key=lambda item: item[1], reverse=True))
+
+            for node in node_rank.keys():
+                if node in companies and rank_num <= 10:
+                    top_companies[rank_num] = node
+                    rank_num += 1
+        else:
+            for node in centrality:
+                if rank_num <= 10:
+                    top_companies[rank_num] = node
+                    rank_num += 1
 
 
-        json.dump(d, open(graph_json, "w"))
+        if nx.is_directed(G):
+            giant_component = G.subgraph(max(nx.strongly_connected_components(G), key=len))
+        else:
+            giant_component = G.subgraph(max(nx.connected_components(G), key=len))
+
+        # nx.draw(giant_component,with_labels=True)
+
+        # save_dir = os.path.join(basedir, 'static/uploads/Giant_Component.png')
+        # plt.savefig(save_dir)
+        #
+        # save_dir1 = os.path.join(basedir, 'static/uploads/Giant_Component1.png')
+        # save_dir2 = os.path.join(basedir, 'static/uploads/Giant_Component2.png')
+
+        # if os.path.exists(save_dir1):
+        #     os.remove(save_dir1)
+        #     plt.savefig(save_dir2)
+        #     img_file = 2
+        # elif os.path.exists(save_dir2):
+        #     os.remove(save_dir2)
+        #     plt.savefig(save_dir1)
+        #     img_file = 1
+        # else:
+        #     plt.savefig(save_dir1)
+        #     img_file = 1
+
 
         analyze = {
+            "giant_component_size": len(giant_component.nodes()),
             "observation_n": form_data['observation_n'],
             "member_nums": len(members),
             "company_nums": len(companies),
             "edge_nums": len(relations_edge),
         }
-        return render_template('dashboard.html', analyze=analyze, dataset=dataset_json)
+
+        return render_template('dashboard.html', analyze=analyze, dataset=dataset_json, top_companies = top_companies)
     else:
         if os.path.exists(graph_json):
             os.remove(graph_json)
